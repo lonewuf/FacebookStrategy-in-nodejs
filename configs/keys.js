4069359fdc2ea799fa513a202c827300a1e41ec7
tawbuts <john.nico.austria@gmail.com>
2020-04-21 01:51:14 -0700

    Ready for deployment

--- configs/keys.js
+++ configs/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   FACEBOOK_APP_ID: "550161245638140",
   FACEBOOK_APP_SECRET: "115da7688100073695e2c2af748ef94c",
-  callbackURL: "http://localhost:4000/auth/facebook/callback",
+  callbackURL: "https://app-fb-node.herokuapp.com/auth/facebook/callback",
   MY_SECRET: "my special secret",
   mongoURI: {
     dev: "mongodb://localhost/fb-node-dev",
