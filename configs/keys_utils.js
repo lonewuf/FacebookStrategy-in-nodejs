8053e93b8df6fec0a55648588f64a1ac4ef71da0
tawbuts <john.nico.austria@gmail.com>
2020-04-22 01:47:56 -0700

    Added callbackURL for localhost in keys_utils

--- configs/keys_utils.js
+++ configs/keys_utils.js
@@ -1,7 +1,7 @@
 module.exports = {
   FACEBOOK_APP_ID: "",
   FACEBOOK_APP_SECRET: "",
-  callbackURL: "",
+  callbackURL: "http://localhost:4000/auth/facebook/callback",
   MY_SECRET: "my special secret",
   mongoURI: {
     dev: "mongodb://localhost/fb-node-dev",
